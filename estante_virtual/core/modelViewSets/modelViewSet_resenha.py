960baddad28600c1d1b494c686477f9a40e6257a
rhuanbrasil <rhuanbrazil@gmail.com>
2025-09-14 09:10:49 -0300

    feat :arrow_up: definindo método para filtrar resenhas por livro

--- estante_virtual/core/modelViewSets/modelViewSet_resenha.py
+++ estante_virtual/core/modelViewSets/modelViewSet_resenha.py
@@ -3,5 +3,16 @@ from ..models import Resenha
 from ..serializers.resenhaSerializer import ResenhaSerializer
 
 class ResenhaViewSet(viewsets.ModelViewSet):
-    queryset = Resenha.objects.all()
     serializer_class = ResenhaSerializer
+
+    def get_queryset(self):
+    
+        queryset = Resenha.objects.all()
+        
+        livro_id = self.request.query_params.get('livro')
+        
+        if livro_id is not None:
+           
+            queryset = queryset.filter(livro__id=livro_id)
+            
+        return queryset
